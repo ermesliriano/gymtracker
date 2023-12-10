b6af98db1fd05cbdf89eb206360b5da7f894e8bf
ermesliriano <ermesliriano@mercedarios.net>
2023-12-10 22:34:56 +0100

    Before introducing the weights and reps spinner

--- app/src/main/java/com/hadash/gymtracker/activities/SessionActivity.kt
+++ app/src/main/java/com/hadash/gymtracker/activities/SessionActivity.kt
@@ -10,15 +10,23 @@ class SessionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_session)
 
-        val spinner: Spinner = findViewById(R.id.exercises)
+        val spinner_ex: Spinner = findViewById(R.id.exercises)
         ArrayAdapter.createFromResource(
             this,
             R.array.exercises_list,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
+            spinner_ex.adapter = adapter
         }
+
+        val repsList = ArrayList<Int>()
+        for (i in 5..30) {
+            repsList.add(i)
+        }
+
+//        val spinner_reps: Spinner = findViewById(R.id.reps)
+//        val adapter = ArrayAdapter(this, android.R.layo)
 
 
     }
